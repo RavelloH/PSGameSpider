365fdf7f36b190e00b0e15f69326424e5a439094
RavelloH <68409330+RavelloH@users.noreply.github.com>
2022-07-03 20:31:28 +0800

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -393,6 +393,43 @@ listend='''
                           class="iconfontsmall icon-annotation"></span>
                     <span onclick="document.getElementById('windowa').innerHTML = aboutfordemo"
                           class="iconfontsmall icon-about"></span>
+			  <br><br>
+
+                    <div><h4>- 更多语言 -</h4>
+
+                    <li><a href="https://ravelloh.github.io/PSGameSpider/">中文</a></li>
+
+                    <li><a href="https://ravelloh.github.io/PSGameSpider/en/">English</a></li>
+
+                    </div>
+
+                    <br>
+
+                    <div><h4>- 评论 -</h4>
+
+                    <br>
+
+                <div id="tcomment"></div>
+
+<script src="https://cdn.staticfile.org/twikoo/1.5.11/twikoo.all.min.js"></script>
+
+<script>
+
+twikoo.init({
+
+  envId: 'https://comment-ravelloh.vercel.app/', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+
+  el: '#tcomment', // 容器元素
+
+  // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
+
+  // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
+
+  // lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/js/utils/i18n/index.js
+
+})
+
+</script>
 
             </section>
             <div class="menu">
