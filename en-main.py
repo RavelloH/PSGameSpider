f3193170a3c1be7124a68a5ddeed0c9df0f9b762
RavelloH <68409330+RavelloH@users.noreply.github.com>
2022-07-03 13:23:17 +0800

    英文适配

---
+++ en-main.py
@@ -0,0 +1,54 @@
+# PlayStation游戏封面爬取
+# Verson:1.2.0
+# Author:RavelloH
+# LICENSE:MIT
+# 爬取内容归索尼及相关工作室所有
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+from wget import download
+import os    
+os.makedirs('./img/',exist_ok=True)
+# 设置目标路径
+link = "store.playstation.com/en-hk/pages/browse/"
+k = 1
+while k < 200:
+    # 爬取
+    html = urlopen("https://"+link+str(k))
+    # 解析
+    obj = bs(html.read(),'html.parser')
+    pic_info = obj.find_all('img')
+    if len(pic_info) == 3:
+        break
+    j = 0 # 配置遍历
+    try:   
+        for i in pic_info:
+            j += 1
+            pic = str(i['src'])
+            name = str(i['alt'])
+            if 'image-no-js' in str(i):
+                if "http" not in pic:
+                    if "data" in pic:
+                        continue
+                    else:
+                        if "//" in pic:
+                            links = "http:"+pic
+                        else:
+                            if pic[0] == "/":
+                                links = "http://"+link+pic
+                            else:
+                                links = "http://"+link+"/"+pic
+            
+                else:
+                    links = pic
+            else:
+                continue
+            # 下载
+            name = download(links,out='./img/'+name+'.jpg')
+            print(name)
+    except:
+        pass
+    for file_name in os.listdir('./img/'):    
+        if '(1)' in file_name:
+            os.remove('./img/' + file_name)
+    k += 1
