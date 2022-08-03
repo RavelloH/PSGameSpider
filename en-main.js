f4fc3e8b6b9349464c08b607e8d71054d8a97689
RavelloH <ravellohh@gmail.com>
2022-08-03 09:57:39 +0800

    Update en-main.js

--- en-main.js
+++ en-main.js
@@ -1,21 +1,21 @@
-var helpfordemo = '<h4><a name="Cannot find the game" href="#Cannot find the game">#Cannot find the game</a></h4><p>Whether there are auto-completion candidates when entering the game name ? <br>The input box comes with candidates, and all games that can be indexed are in the candidate box. If there is a game you want to find in the candidate box, please click to enter it and search. </p><p>Is the search correct? <br>Please check that your input is correct. What is entered will be echoed in the output box. </p><p>Is the game name in another language? <br>Currently the program only supports official Chinese translation (if there is no Chinese translation, English will be used automatically). </p><p>Is the game released on PlayStation? <br>Crawler for other platforms is under development, please confirm if there is a game to be searched in PlayStationStore</p><p>Is the release time later than the generation time of this page? (See the upper right for the generation time)<br>This crawler crawls once a day by default. If you want to update it now, please <a href="https://github.com/ravelloh/PSGameSpider" class="linkline">Go to the Github repository </a>, Star this warehouse (after triggering Workflows, Star can be recovered) </p>'
-var aboutfordemo = '<h4><a name="About" href="#About">#About </p>'
-var aboutfordemo = '<h4><a name="About" a></h4><p>Project Name:PSGameSpider</p><p>Author:RavelloH</p>'
+var helpfordemo = '<h4><a name="无法找到游戏" href="#无法找到游戏">#无法找到游戏</a></h4><p>输入游戏名时是否有自动补全候选？<br>输入框自带候选，所有能被索引到的游戏都在候选框中。若候选框中有需要查找的游戏，请直接点击将其输入并搜索。</p><p>搜索内容是否正确？<br>请检查输入的内容是否正确。输入的内容将回显在输出框中。</p><p>游戏名是否为其他语言？<br>目前程序只支持官方中文翻译（若无中文翻译则自动使用英文）。</p><p>游戏是否在PlayStation中发布？<br>其他平台的爬虫正在制作中，请确认PlayStationStore中是否有要搜索的游戏</p><p>发布时间是否晚于此页面生成时间？(生成时间见右上)<br>此爬虫默认一天爬取一次，如需现在更新，请<a href="https://github.com/ravelloh/PSGameSpider" class="linkline">转到Github仓库</a>中，Star此仓库（在触发Workflows后，可将Star收回）</p>'
+var aboutfordemo = '<h4><a name="关于" href="#关于">#关于</a></h4><p>项目名:PSGameSpider</p><p>作者:RavelloH</p><p>代码许可:MIT</p><p>开源:<a href="https://github.com/ravelloh/PSGameSpider" class="linkline">转到Github仓库</a></p>'
+document.getElementById('output').innerHTML = "终端正在等待数据输入...";
 var innerimghead = '<figure class="hover-menu"><img src="img/';
 var innerimgparta = '.jpg"/><div><a href="img/';
-var innerimgpartaend = '.jpg ">Preview image</a><a href="https://github.com/RavelloH/PSGameSpider/blob/main/img/'
-var innerimgpartb = '.jpg">View source</a><a href= "https://store.playstation.com/zh-hans-cn/search/';
-var innerimgpartc = '">Go to Store</a></div></figure>';
+var innerimgpartaend = '.jpg">预览图片</a><a href="https://github.com/RavelloH/PSGameSpider/blob/main/img/'
+var innerimgpartb = '.jpg">查看源</a><a href="https://store.playstation.com/zh-hans-cn/search/';
+var innerimgpartc = '">转到Store</a></div></figure>';
 function search() {
         var innerimgurl = document.getElementById("searchurl").value;
         if (totalgamelist.includes(innerimgurl)) {
                 innerover = innerimghead + innerimgurl + innerimgparta + innerimgurl + innerimgpartaend + innerimgurl + innerimgpartb + innerimgurl + innerimgpartc;
                 document.getElementById('windowa').innerHTML = innerover;
-                document.getElementById('searchtitle').innerHTML = "- Search " + innerimgurl + " -";
-                document.getElementById('output').innerHTML = 'The query succeeded(' + innerimgurl + ') Touch the left cover to see more content. <br>If nothing appears, please wait for the image to load. ';
+                document.getElementById('searchtitle').innerHTML = "- 搜索 " + innerimgurl + " -";
+                document.getElementById('output').innerHTML = '查询成功(' + innerimgurl + ') 触摸左侧封面查看更多内容。<br>若无显示，请稍等以使图片加载。';
         } else {
                 var timedata = new Date();
-                var errora = 'Data not found (' + innerimgurl + '). Please confirm:<br>1. Is there auto-completion when entering the game name? <br>2. Is the search content correct? <br>3. Is the game name in another language? <br>4. Is the game released in PlayStation? <br>5. Is the publication time later than the generation time of this page? (See above for generation time)<br>Click the function bar below to view help';
+                var errora = '未找到相应数据(' + innerimgurl + ')。请确认:<br>1.输入游戏名时是否有自动补全？<br>2.搜索内容是否正确？<br>3.游戏名是否为其他语言？<br>4.游戏是否在PlayStation中发布？<br>5.发布时间是否晚于此页面生成时间？(生成时间见上)<br>点击下方功能栏查看帮助';
                 var timenow = "[" + timedata.getHours() + ":" + timedata.getMinutes() + ":" + timedata.getSeconds() + "]";
                 document.getElementById('output').innerHTML = timenow + errora;
         }
@@ -31,8 +31,8 @@ function quicksearch(name) {
         var innerimgurl = name;
         innerover = innerimghead + innerimgurl + innerimgparta + innerimgurl + innerimgpartaend + innerimgurl + innerimgpartb + innerimgurl + innerimgpartc;
         document.getElementById('windowa').innerHTML = innerover;
-        document.getElementById('searchtitle').innerHTML = "- Search" + innerimgurl + " -";
-        document.getElementById('output').innerHTML = 'Search success(' + innerimgurl + ') touch left cover See more. <br>If nothing appears, please wait for the image to load. ';
+        document.getElementById('searchtitle').innerHTML = "- 搜索 " + innerimgurl + " -";
+        document.getElementById('output').innerHTML = '查询成功(' + innerimgurl + ') 触摸左侧封面查看更多内容。<br>若无显示，请稍等以使图片加载。';
 }
 
 function drop() {
@@ -45,7 +45,6 @@ function drop() {
 }
 setTimeout("drop()", "2000");
 twikoo.init({
-        envId: 'https:// comment-ravelloh.vercel.app/', // Fill in envId for Tencent cloud environment; fill in address for Vercel environment (https://xxx.vercel.app) 
-        el: '#tcomment',
-        lang: 'en-us'
+  envId: 'https://comment-ravelloh.vercel.app/', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+  el: '#tcomment',
 })
