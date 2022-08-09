7d0955539198bf5c2102105f8e43d6f7827b2c05
RavelloH <ravellohh@gmail.com>
2022-08-09 10:28:58 +0800

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -474,7 +474,7 @@ content = f1.read()
 f1.close()
 
 t = content.replace("\n","")
-soup = bs.BeautifulSoup(t,features="lxml")
+soup = bs(t,features="lxml")
 soup.prettify()
 print(soup.prettify())
 with open("index.html","w") as f2:
