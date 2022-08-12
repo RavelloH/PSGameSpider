bb5ea81ec164ef72f182a50843ef08f754f600af
RavelloH <ravellohh@gmail.com>
2022-08-12 12:03:24 +0800

    Create thumbs.py

---
+++ thumbs.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+
+os.makedirs('min-recent',exist_ok=True)
+for j in os.listdir('min-recent/'):
+    os.remove('min-recent/'+j)
+def smallPic():
+    for i in os.listdir('recent/'):
+        filename = "recent/"+i
+        print(filename)
+        im = Image.open(filename)
+        print(im.format, im.size, im.mode, filename)
+        im.thumbnail((128, (im.size[1] * im.size[0] / 128)))
+        savepath = "min-recent/"+i
+        im.save(savepath, 'JPEG')
+        
+if __name__ == '__main__':
+    smallPic()
