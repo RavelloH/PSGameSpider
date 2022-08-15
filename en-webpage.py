4c903a74d8758744c7eabd8f9c71e86cd9ce024d
RavelloH <ravellohh@gmail.com>
2022-08-15 11:59:59 +0800

    Update en-webpage.py

--- en-webpage.py
+++ en-webpage.py
@@ -416,7 +416,7 @@ listend='''
 <script src="https://cdn.staticfile.org/twikoo/1.6.4/twikoo.all.min.js"></script>
 
 <script>
-window.TWIKOO_MAGIC_PATH="PSGameSpider/en"
+window.TWIKOO_MAGIC_PATH="/PSGameSpider/en";
 twikoo.init({
 
   envId: 'https://comment-ravelloh.vercel.app
