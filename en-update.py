0a4d38e539f6f60abddb5f6fd5cc71ea2121da58
RavelloH <ravellohh@gmail.com>
2023-01-31 15:18:14 +0800

    Update en-update.py

--- en-update.py
+++ en-update.py
@@ -11,18 +11,18 @@ import os
 import re
 import html
 # 创建目录
-os.makedirs('./img/',exist_ok=True)
-os.makedirs('./recent/',exist_ok=True)
+os.makedirs('./img-en/',exist_ok=True)
+os.makedirs('./recent-en/',exist_ok=True)
 # 设置目标路径
-origin_link = "https://store.playstation.com/zh-hans-hk/pages/latest"
+origin_link = "https://store.playstation.com/en-us/pages/latest"
 # 删除重复文件
-for file_name in os.listdir('./img/'):
+for file_name in os.listdir('./img-en/'):
     if '(1)' in file_name or '(2)' in file_name or '(3)' in file_name:
-        os.remove('./img/' + file_name)
+        os.remove('./img-en/' + file_name)
         print("清除重复文件:"+file_name)
 # 清空列表
-for recent_file_name in os.listdir('./recent/'):
-    os.remove('./recent/' + recent_file_name)
+for recent_file_name in os.listdir('./recent-en/'):
+    os.remove('./recent-en/' + recent_file_name)
     print("清空最近更新列表:"+recent_file_name)
 # 爬取最新更新列表链接
 origin_html = urlopen(origin_link)
@@ -47,8 +47,8 @@ while k < 20:
             img = imgs[0]['src']
             names = i.find_all('section','psw-product-tile__details psw-m-t-2')
             name = re.sub("\(.*\)", "", str(names[0].contents[1].string), count=0, flags=0)
-            download(img,out='./img/'+name+'.jpg',bar='')
-            download(img,out='./recent/'+name+'.jpg',bar='')
+            download(img,out='./img-en/'+name+'.jpg',bar='')
+            download(img,out='./recent-en/'+name+'.jpg',bar='')
             print("[I]Success: "+name)
         except:                        
             print('[E]Error: %s' %(name))
@@ -56,27 +56,27 @@ while k < 20:
     print("爬取:第"+str(k)+"页完成")
     k += 1
 # 删除重复文件
-for file_name in os.listdir('./img/'):
+for file_name in os.listdir('./img-en/'):
     if '(1)' in file_name:
-        os.remove('./img/' + file_name)
+        os.remove('./img-en/' + file_name)
         print("清除总重复文件:"+file_name)
-for file_name in os.listdir('./recent/'):
+for file_name in os.listdir('./recent-en/'):
     if '(1)' in file_name:
-        os.remove('./recent/' + file_name)
+        os.remove('./recent-en/' + file_name)
         print("清除最近重复文件:"+file_name)
 
-for file_name in os.listdir('./recent/'):
+for file_name in os.listdir('./recent-en/'):
     try:
         file_name_new = re.sub(r'"', "", file_name)
         file_name_new = re.sub(r",", "",file_name_new)
-        os.rename("./recnet/"+file_name,"./recent/"+file_name_new)
+        os.rename("./recnet-en/"+file_name,"./recent-en/"+file_name_new)
     except:
-        print('Error:recent重命名错误')
+        pass
     
-for file_name in os.listdir('./img/'):
+for file_name in os.listdir('./img-en/'):
     try:
         file_name_new = re.sub(r'"',"",file_name)
         file_name_new = re.sub(r",","",file_name_new)
-        os.rename("./img/"+file_name,"./img/"+file_name_new)
+        os.rename("./img-en/"+file_name,"./img-en/"+file_name_new)
     except:
-        print('Error:img重命名错误'+file_name)
+        pass
