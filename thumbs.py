c05d1035a77f4a34fa1fbd4538768226c1893e81
RavelloH <ravellohh@gmail.com>
2023-04-16 18:48:49 +0800

    Update thumbs.py

--- thumbs.py
+++ thumbs.py
@@ -6,14 +6,17 @@ os.makedirs('min-recent',exist_ok=True)
 for j in os.listdir('min-recent/'):
     os.remove('min-recent/'+j)
 def smallPic():
-    for i in os.listdir('recent/'):
-        filename = "recent/"+i
-        print(filename)
-        im = Image.open(filename)
-        print(im.format, im.size, im.mode, filename)
-        im.thumbnail((256, (im.size[1] * im.size[0] / 256)))
-        savepath = "min-recent/"+i
-        im.save(savepath, 'JPEG')
+    try:
+        for i in os.listdir('recent/'):
+            filename = "recent/"+i
+            print(filename)
+            im = Image.open(filename)
+            print(im.format, im.size, im.mode, filename)
+            im.thumbnail((256, (im.size[1] * im.size[0] / 256)))
+            savepath = "min-recent/"+i
+            im.save(savepath, 'JPEG')
+    except:
+        pass
         
 if __name__ == '__main__':
     smallPic()
