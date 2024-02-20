6d403667f72bf58bb3f01b8be11178aec43fccee
RavelloH <hyh20060327@qq.com>
2024-02-20 13:42:21 +0800

    Fix. 修复合并流程

--- program/fetchAll.js
+++ program/fetchAll.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const crypto = require('crypto');
 const axios = require('axios');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 
 const rootPath = 'https://store.playstation.com';
 const langList = ['zh-hans-hk', 'en-us', 'ja-jp', 'de-de'];
@@ -19,27 +19,27 @@ async function starter() {
     gameListOld = getData('data/gameList.json');
     // 元数据获取
     for (let i = 0; i < langList.length; i++) {
-        resultPage = 0
-        rlog.info(`Start fetching all games in ${langList[i]}`)
-        await getList(langList[i])
+        resultPage = 0;
+        rlog.info(`Start fetching all games in ${langList[i]}`);
+        await getList(langList[i]);
     }
-    exportGameList(mergeObjects(gameListOld, gameList), 'data/gameList.json')
+    exportGameList(mergeObjects(gameListOld, gameList), 'data/gameList.json');
     gameList = getData('data/gameList.json');
     rlog.success('Metedata successfully fetched.');
     rlog.info('Start downloading images...');
     // 封面下载
-    let newResult = await downloadImages(gameList2);
+    let newResult = await downloadImages(gameList);
     gameListOld = getData('data/gameList.json');
-    exportGameList(mergeObjects(newResult, gameListOld), 'data/gameList.json')
-    rlog.success('Successfully downloaded images')
-    rlog.info('Start get more informations...')
+    exportGameList(mergeObjects(newResult, gameListOld), 'data/gameList.json');
+    rlog.success('Successfully downloaded images');
+    rlog.info('Start get more informations...');
     // 元数据扩充
-    return
+    return;
     gameList = getData('data/gameList.json');
 
-    let gameListNew = await getInfo(gameList)
-    exportGameList(mergeObjects(gameList, gameListNew), 'data/gameList.json')
-    rlog.success('All jobs finished')
+    let gameListNew = await getInfo(gameList);
+    exportGameList(mergeObjects(gameList, gameListNew), 'data/gameList.json');
+    rlog.success('All jobs finished');
 }
 // 烤串转驼峰
 function kebabToCamel(str) {
@@ -61,10 +61,14 @@ function mergeObjects(oldObj, newObj) {
     let mergedObj = [...oldObj];
 
     for (let newItem of newObj) {
-        let existingItemIndex = mergedObj.findIndex(item => item.path === newItem.path);
+        let existingItemIndex = mergedObj.findIndex((item) => item.path === newItem.path);
 
         if (existingItemIndex !== -1) {
-            mergedObj[existingItemIndex] = { ...mergedObj[existingItemIndex], ...newItem, img: mergedObj[existingItemIndex].img };
+            mergedObj[existingItemIndex] = {
+                ...mergedObj[existingItemIndex],
+                ...newItem,
+                img: mergedObj[existingItemIndex].img,
+            };
         } else {
             mergedObj.push(newItem);
         }
@@ -91,27 +95,31 @@ async function getList(lang) {
                 if (error) {
                     rlog.exit(error);
                 } else {
-                    let $ = res.$;
+                    try {
+                        let $ = res.$;
 
-                    // 获取所有游戏列表
-                    let imgList = $(
-                        '#main > div > section > div > div > div > div.psw-l-w-3\\/4 > div.psw-l-w-1\\/1 > ul',
-                    );
-                    imgList.find('li').each(function (i, e) {
-                        let path = $(this).find('a').attr('href');
-                        let img = $(this).find('img').attr('src').split('?')[0];
-                        let name = $(this)
-                        .find(
-                            'div > a > div > section > span.psw-t-body.psw-c-t-1.psw-t-truncate-2.psw-m-b-2',
-                        )
-                        .text();
-                        gameList.push({
-                            name: name,
-                            img: img,
-                            path: rootPath + path,
-                            lang: lang,
+                        // 获取所有游戏列表
+                        let imgList = $(
+                            '#main > div > section > div > div > div > div.psw-l-w-3\\/4 > div.psw-l-w-1\\/1 > ul',
+                        );
+                        imgList.find('li').each(function (i, e) {
+                            let path = $(this).find('a').attr('href');
+                            let img = $(this).find('img').attr('src').split('?')[0];
+                            let name = $(this)
+                                .find(
+                                    'div > a > div > section > span.psw-t-body.psw-c-t-1.psw-t-truncate-2.psw-m-b-2',
+                                )
+                                .text();
+                            gameList.push({
+                                name: name,
+                                img: img,
+                                path: rootPath + path,
+                                lang: lang,
+                            });
                         });
-                    });
+                    } catch (e) {
+                        rlog.error(e);
+                    }
 
                     // 获取总数
                     if (!allPage) {
@@ -134,10 +142,9 @@ async function getList(lang) {
 
         c.queue(`${rootPath}/${lang}/pages/browse/1`);
 
-        c.on('drain',
-            () => {
-                resolve();
-            });
+        c.on('drain', () => {
+            resolve();
+        });
     });
 }
 
@@ -154,7 +161,7 @@ const downloadImages = async (json) => {
     const newJson = [];
     for (let i = 0; i < json.length; i++) {
         const item = json[i];
-        const imgUrl = item.img;;
+        const imgUrl = item.img;
         const imgName = crypto.createHash('md5').update(imgUrl).digest('hex');
         const imgPath = path.join(imagesPath, `${imgName}.jpg`);
 
@@ -167,7 +174,9 @@ const downloadImages = async (json) => {
         let retryCount = 0;
         while (!success && retryCount < 3) {
             try {
-                rlog.info(`${newJson.length+1} / ${json.length} Fetching img for ${item['name']}...`);
+                rlog.info(
+                    `${newJson.length + 1} / ${json.length} Fetching img for ${item['name']}...`,
+                );
                 const response = await axios({
                     url: imgUrl,
                     method: 'GET',
@@ -185,12 +194,14 @@ const downloadImages = async (json) => {
                 downloadedImages[imgUrl] = path.join('images', `${imgName}.jpg`);
                 item.img = downloadedImages[imgUrl];
                 newJson.push(item);
-                exportGameList(mergeObjects(newJson, gameListOld), 'data/gameList.json')
+                exportGameList(mergeObjects(newJson, gameListOld), 'data/gameList.json');
                 success = true;
             } catch (error) {
                 retryCount++;
                 rlog.warning(
-                    `Error downloading image ${imgUrl}. Retrying in ${800 * retryCount}ms... Error:${error}`,
+                    `Error downloading image ${imgUrl}. Retrying in ${
+                        800 * retryCount
+                    }ms... Error:${error}`,
                 );
                 await new Promise((resolve) => setTimeout(resolve, 800 * retryCount));
             }
@@ -205,42 +216,44 @@ const downloadImages = async (json) => {
 };
 
 async function getInfo(gameList) {
-    for (let i= 0; i< gameList.length; i++) {
-        rlog.log(`${i+1} / ${gameList.length} Fetching informations for ${gameList[i].name}`)
-        let url = gameList[i].path
-        let infoJson = await getInfoJson(url)
+    for (let i = 0; i < gameList.length; i++) {
+        rlog.log(`${i + 1} / ${gameList.length} Fetching informations for ${gameList[i].name}`);
+        let url = gameList[i].path;
+        let infoJson = await getInfoJson(url);
         gameList[i].fullname = infoJson.name;
         gameList[i].category = infoJson.category;
         gameList[i].description = infoJson.description;
-        gameList[i].price = infoJson.offers.price
-        gameList[i].priceCurrency = infoJson.offers.priceCurrency
-        gameList[i].priceHistory = !gameList[i].priceHistory ? [[getTodayDate(),gameList[i].price]] : gameList[i].priceHistory.concat([getTodayDate(),gameList[i].price])
+        gameList[i].price = infoJson.offers.price;
+        gameList[i].priceCurrency = infoJson.offers.priceCurrency;
+        gameList[i].priceHistory = !gameList[i].priceHistory
+            ? [[getTodayDate(), gameList[i].price]]
+            : gameList[i].priceHistory.concat([getTodayDate(), gameList[i].price]);
     }
-    return gameList
+    return gameList;
 }
 
 const getInfoJson = async (url, retryCount = 0) => {
-  try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    return JSON.parse($('#mfe-jsonld-tags').text());
-  } catch (error) {
-    if (retryCount < 30) {
-      rlog.warning(`Error getting info JSON from ${url}. Retrying in 1 second...`);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      return getInfoJson(url, retryCount + 1);
-    } else {
-      rlog.error(`Failed to get info JSON from ${url} after 3 retries. Skipping...`);
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        return JSON.parse($('#mfe-jsonld-tags').text());
+    } catch (error) {
+        if (retryCount < 30) {
+            rlog.warning(`Error getting info JSON from ${url}. Retrying in 1 second...`);
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            return getInfoJson(url, retryCount + 1);
+        } else {
+            rlog.error(`Failed to get info JSON from ${url} after 3 retries. Skipping...`);
+        }
     }
-  }
 };
 
 const getTodayDate = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const day = today.getDate();
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
 
-  return `${year}/${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}`;
+    return `${year}/${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}`;
 };
 starter();
