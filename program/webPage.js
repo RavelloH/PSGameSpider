d654f9ce6cc40bafae16421e7ce97d5ba2a2a69d
RavelloH <hyh20060327@qq.com>
2024-02-22 20:08:14 +0800

    Fix. 修复降价判断

--- program/webPage.js
+++ program/webPage.js
@@ -1,9 +1,8 @@
-const rbuild = require('render-build')
-const RLog = require('rlog-js')
-const rlog = new RLog()
-const fs = require('fs-extra')
-const ejs = require('ejs')
-
+const rbuild = require('render-build');
+const RLog = require('rlog-js');
+const rlog = new RLog();
+const fs = require('fs-extra');
+const ejs = require('ejs');
 
 function getData(filepath) {
     if (fs.existsSync(filepath)) {
@@ -16,15 +15,14 @@ function getData(filepath) {
     }
 }
 
-
 const langList = ['zh-hans-hk', 'en-us', 'ja-jp', 'de-de'];
-const config = getData('./config.json')
-const gameList = getData('./data/gameList.json')
+const config = getData('./config.json');
+const gameList = getData('./data/gameList.json');
 
 const getRecentlyReleasedGames = (games, daysAgo = 14) => {
     const today = new Date();
 
-    const recentGames = games.filter(game => {
+    const recentGames = games.filter((game) => {
         const releaseDate = new Date(game.releaseTime);
         const diffInMs = Math.abs(releaseDate - today);
         const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365);
@@ -35,29 +33,41 @@ const getRecentlyReleasedGames = (games, daysAgo = 14) => {
     return recentGames;
 };
 
-const getRecentlyDiscountedGames = (games, daysAgo = 14) => {
+const getRecentlyDiscountedGames = (games, daysAgo = 7) => {
     const today = new Date();
 
-    const discountedGames = games.filter(game => {
+    const discountedGames = games.filter((game) => {
         const priceHistory = game.priceHistory;
-        const latestPrice = priceHistory[priceHistory.length - 1][1];
 
-        if (priceHistory.length < 2 || latestPrice === null) {
+        if (priceHistory.length < 2) {
             return false;
         }
 
-        for (let i = priceHistory.length - 2; i >= 0; i--) {
-            if (priceHistory[i][1] !== latestPrice && priceHistory[i][1] !== null) {
-                const previousPrice = priceHistory[i][1];
-                const discountDate = new Date(priceHistory[priceHistory.length - 1][0]);
-                const diffInMs = Math.abs(discountDate - today);
-                const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365);
+        const latestPrice = priceHistory[priceHistory.length - 1][1];
+        if (latestPrice === null) {
+            return false;
+        }
 
-                if (diffInYears <= daysAgo / 365) {
-                    game.previousPrice = previousPrice;
-                    return true;
-                }
+        let previousPrice = null;
+        let foundDiscount = false;
+
+        for (let i = priceHistory.length - 2; i >= 0; i--) {
+            const price = priceHistory[i][1];
+            const date = new Date(priceHistory[i][0]);
+
+            const diffInMs = Math.abs(today - date);
+            const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
+
+            if (diffInDays <= daysAgo && price > latestPrice) {
+                previousPrice = price;
+                foundDiscount = true;
+                break;
             }
+        }
+
+        if (foundDiscount) {
+            game.previousPrice = previousPrice;
+            return true;
         }
 
         return false;
@@ -72,11 +82,6 @@ function convertObjectToFile(obj, outputPath) {
     rlog.success(`Object successfully converted and saved to ${outputPath}`);
 }
 
-
-
-
-
-
 function getRandomItems(inputList) {
     const randomItems = [];
     if (inputList.length <= 100) {
@@ -90,7 +95,7 @@ function getRandomItems(inputList) {
     return randomItems;
 }
 
-let template
+let template;
 
 // 修改默认行为
 rbuild.build = async function (rootPath) {
@@ -99,11 +104,14 @@ rbuild.build = async function (rootPath) {
         return;
     }
     rlog.log('Start building...');
-    
-    let preTemplate = await rbuild.singleBuild(fs.readFileSync('template/layout.html', 'utf-8'), 'template/')
 
-    rlog.success('Fetched templates')
-    
+    let preTemplate = await rbuild.singleBuild(
+        fs.readFileSync('template/layout.html', 'utf-8'),
+        'template/',
+    );
+
+    rlog.success('Fetched templates');
+
     // 创建函数
     convertObjectToFile(gameList, 'data/gameList.js');
 
@@ -131,57 +139,55 @@ rbuild.build = async function (rootPath) {
     }
     // 首页构建
     let doc = fs.readFileSync('template/index.ejs', 'utf-8');
-    let config = rbuild.config.page
-    config.doc = doc
-    config.title = '索引'
-    config.keywords = 'playstation'
-    config.description = 'PSGameSpider'
-    config.pagetype = 'edge'
-    config.url = rbuild.config.siteUrl
-    config.pageJs = `<script>${fs.readFileSync('origin/index.js', 'utf-8')}</script>`
-    config.prefetch = []
-    config.randomList = getRandomItems(gameList)
-    config.gameList = gameList
-    config.recent = getRecentlyReleasedGames(gameList)
-    config.discount = getRecentlyDiscountedGames(gameList)
+    let config = rbuild.config.page;
+    config.doc = doc;
+    config.title = '索引';
+    config.keywords = 'playstation';
+    config.description = 'PSGameSpider';
+    config.pagetype = 'edge';
+    config.url = rbuild.config.siteUrl;
+    config.pageJs = `<script>${fs.readFileSync('origin/index.js', 'utf-8')}</script>`;
+    config.prefetch = [];
+    config.randomList = getRandomItems(gameList);
+    config.gameList = gameList;
+    config.recent = getRecentlyReleasedGames(gameList);
+    config.discount = getRecentlyDiscountedGames(gameList);
     doc = ejs.render(preTemplate, config);
-    doc = ejs.render(doc, config)
+    doc = ejs.render(doc, config);
     // 保存文件
     rbuild.writeFile('public/index.html', doc);
-    rlog.log('Successfully built index')
-
+    rlog.log('Successfully built index');
 
     let templateDoc = fs.readFileSync('template/item.ejs', 'utf-8');
-    config = {}
+    config = {};
     // 遍历构建
     try {
         for (let i = 0; i < gameList.length; i++) {
             rlog.info(`Building ${gameList[i].name}...`);
 
-
             // 配置合并
             config = rbuild.config.page;
             config = rbuild.mergeObjects(config, gameList[i]);
             config.doc = templateDoc;
-            config.title = gameList[i].fullname
+            config.title = gameList[i].fullname;
             config.keywords = gameList[i].keywords || '';
             config.description = gameList[i].description || '';
             config.pagetype = gameList[i].pagetype || 'edge';
-            config.url = config.siteUrl + gameList[i].lang + '/' + gameList[i].name
-            config.game = gameList[i]
+            config.url = config.siteUrl + gameList[i].lang + '/' + gameList[i].name;
+            config.game = gameList[i];
             config.pageJs = `<script>function main() {
                 loadComment('/PSGameSpider'+location.pathname)
-            }</script>`
+            }</script>`;
             config.prefetch = [];
 
             doc = ejs.render(preTemplate, config);
-            doc = ejs.render(doc,config)
+            doc = ejs.render(doc, config);
 
             // 保存文件
             rbuild.writeFile(
                 rbuild.processPath(
                     rbuild.config.outputDirectory,
-                    gameList[i].lang + '/'+gameList[i].name + '/index.html' 
+                    gameList[i].lang + '/' + gameList[i].name + '/index.html',
                 ),
                 doc,
             );
@@ -191,6 +197,6 @@ rbuild.build = async function (rootPath) {
         rlog.exit(e);
         return;
     }
-}
+};
 
-rbuild.build('.')
+rbuild.build('.');
